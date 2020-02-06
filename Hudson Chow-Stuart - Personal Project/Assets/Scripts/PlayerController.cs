9fc6b021215eac0159b62bc5f2c46d42c7272e77
huskystuart <hc01843@srcs.org>
2020-02-06 15:35:55 -0800

    Lab 3 in progress

---
+++ Hudson Chow-Stuart - Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1;
+    public float rotspeed = 5;
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
