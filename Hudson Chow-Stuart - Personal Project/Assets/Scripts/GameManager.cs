a17104c79751443429c8be8597c935ad9a50bd52
huskystuart <hc01843@srcs.org>
2020-03-10 15:05:15 -0700

    Lab 5 Swap out your Assets 1:30

---
+++ Hudson Chow-Stuart - Personal Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> findObjects;
+    public Transform selectedObject;
+
+    //the difference between selectedObject and selected is that selectedObject contains the GUI object that the selected object (not selectedObject) will be attached to
+    //while selected contains a reference to the actual object
+    private GameObject selected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
